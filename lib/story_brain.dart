1ddcbebd979f5d52ad2fede7454e233d2ef5b84b
Ahmed EL-Dakhakhny <>
2020-06-23 11:02:41 +0200

    creating get story function

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -32,10 +32,10 @@ class StoryBrain {
         choice1: 'Restart',
         choice2: '')
   ];
+  int currentStoryNumber = 0;
+  String getStory() => _storyData[currentStoryNumber].storyTitle;
 }
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
